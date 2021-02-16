1befc9a77801954abfba9bd12ee93ac573bf5802
ayano kobayashi <ultrakobayashiayano@yahoo.co.jp>
2021-02-16 14:32:08 +0900

    クチコミ一覧のリンク修正

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,22 +3,28 @@ class ReviewsController < ApplicationController
 
   def index
     @reviews = Review.all
+    @shop = Shop.find(params[:shop_id])
   end
 
   def new
     @review = Review.new
+    @review.shop_id = params[:shop_id]
   end
 
   def create
+      binding.irb
     @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    @review.shop_id = params[:shop_id]
     if @review.save
-      redirect_to reviews_path, notice: '新規登録しました'
+      redirect_to shop_reviews_path, notice: '新規登録しました'
     else
       render :new
     end
   end
 
   def show
+    @shop = Shop.find(params[:shop_id])
   end
 
   def edit
