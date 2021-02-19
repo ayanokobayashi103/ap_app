0dbaee0ee99df81579f73237bca6a2b53f524724
ayano kobayashi <ultrakobayashiayano@yahoo.co.jp>
2021-02-20 00:11:54 +0900

    seedデータ20件作成

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -9,5 +9,5 @@ class Owner < ApplicationRecord
   validates :company, presence: true
   validates :tel, presence: true, format: { with: VALID_TEL_REGEX }
   validates :postcode, presence: true, format: { with: VALID_POSTCODE_REGEX }
-  has_many :shops
+  has_many :shops, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,78 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Owner.destroy_all
+Shop.destroy_all
+Review.destroy_all
+
+n = 1
+m = 0
+# User
+20.times do |n|
+  n += 1
+  name = Faker::Name.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               id: n,
+               )
+end
+
+# # Owner
+20.times do |n|
+  n += 1
+  name = Faker::Artist.name
+  email = Faker::Internet.email
+  password = "password"
+  company = Faker::Company.name
+  tel = "0123456789"
+  postcode = "1234567"
+  Owner.create!(name: name,
+               email: email,
+               password: password,
+               company: company,
+               tel: tel,
+               postcode: postcode,
+               id: n
+               )
+end
+
+# Shop
+20.times do |n|
+  n += 1
+  name = Faker::Book.title
+  brand = Faker::Beer.brand
+  address = Faker::Address.full_address
+  detail = Faker::JapaneseMedia::StudioGhibli.quote
+  Shop.create!(name: name,
+               brand: brand,
+               address: address,
+               start_dt: "10:00",
+               end_dt: "18:00",
+               detail: detail,
+               owner_id: n,
+               url: "",
+               contact_detail: "",
+               id: n
+               )
+end
+
+# Review
+20.times do |n|
+  n += 1
+  content = Faker::Restaurant.review
+  Review.create!(content: content,
+                shop_id: n,
+                user_id: n,
+                )
+end
+
+10.times do |n|
+  n += 1
+  m += 2
+  content = Faker::JapaneseMedia::StudioGhibli.quote
+  Review.create!(content: content,
+                shop_id: n,
+                user_id: m,
+                )
+end
